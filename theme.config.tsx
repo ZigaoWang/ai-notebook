2abf94f042379062b44b377df252ae4bf2798edb
Zigao Wang <zigao.wang24@gmail.com>
2024-07-21 10:17:30 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>AIGC 设计创意新未来</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ZigaoWang/aigc',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/ZigaoWang/aigc',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'AIGC 设计创意新未来',
   },
 }
 
