77f9446573ff0cb836249484b7c2aca0deae0ffb
Zigao Wang <zigao.wang24@gmail.com>
2024-07-23 15:11:03 +0800

    Added logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Zigao 的 AI 笔记本</span>,
+  logo: (
+      <div className={"flex flex-row items-center justify-center gap-1.5"}>
+        <Image
+            src={"https://assets.zigao.wang/img/AIGC-logo.png"}
+            alt={"Logo"}
+            width={512}
+            height={512}
+            className={"h-6 w-6 -translate-y-[1px]"}
+        />
+        <span className={"text-lg font-bold"}>Zigao 的 AI 笔记本</span>
+      </div>
+  ),
   project: {
     link: 'https://github.com/ZigaoWang/ai-notebook',
   },
