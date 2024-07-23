b29351fe6cb0422331e4909875b355f0971006ea
Zigao Wang <zigao.wang24@gmail.com>
2024-07-23 15:16:48 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import Image from 'next/image'
 
 const config: DocsThemeConfig = {
   logo: (
       <div className="flex items-center gap-2">
-        <Image
+        <img
             src="https://assets.zigao.wang/img/AIGC-logo.png"
             alt="Logo"
-            width={24}  // Adjusted width
-            height={24} // Adjusted height
-            className="object-contain" // Ensures the image scales properly
+            width={24}
+            height={24}
+            className="object-contain"
         />
         <span className="text-lg font-bold">Zigao 的 AI 笔记本</span>
       </div>
